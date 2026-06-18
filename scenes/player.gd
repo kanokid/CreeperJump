a67dc492f7b91aee3fb5a75929c8e1b4074a70bd
kanokid <57643139+kanokid@users.noreply.github.com>
2026-06-18 16:45:51 -0400

    finished game

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,14 +1,14 @@
 extends CharacterBody2D
 
 
-const SPEED = 600.0
-const JUMP_VELOCITY = -700.0
+const SPEED = 850.0
+const JUMP_VELOCITY = -800.0
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += get_gravity() * 1.25 * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
